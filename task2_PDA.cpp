d8d738946a5068e8f9a81168c87df96d2d7c1570
Eman  Elharery <106907391+Eman-Elharery@users.noreply.github.com>
2025-05-14 21:48:18 +0300

    Update task2_PDA.cpp

--- task2_PDA.cpp
+++ task2_PDA.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void testing ();
 bool accepted (string s)
 {
     if(s.size()%2)
@@ -28,9 +28,33 @@ bool accepted (string s)
   string str;
   cin>>str;
   if(accepted(str))
-  cout<<"Accepted";
+  cout<<"Accepted"<<endl;
   else
-  cout<<"Rejected";
+  cout<<"Rejected"<<endl;
+  testing();
     return 0;
+}
+
+////////////////////////////////testing ///////////////////////////
+
+void testing ()
+{
+vector<pair<string, string>> test_cases = {
+    {"1100", "Rejected"},
+    {"101", "Accepted"},
+    {"1111", "Rejected"},
+    {"110111", "Rejected"},
+    {"01010", "Accepted"}
+};
+for(int i=0;i<test_cases.size();i++)
+{
+    cout<<"Input : "<< test_cases[i].first<<endl;
+    cout<<"Excepted Output : "<<test_cases[i].second<<endl;
+    cout<<"Excepted Output : ";
+    if(accepted(test_cases[i].first))
+     cout<<"Accepted"<<endl;
+      else
+     cout<<"Rejected"<<endl;
+}
 }
 
